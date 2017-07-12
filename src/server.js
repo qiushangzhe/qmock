8df0a627c44eb4c15de4bfc54af757dc652afdd3
qiushangzhe <qiusz@jiqdaibao.com>
2017-07-12 14:37:42 +0800

    cross

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ var app = express();
 app.use(bodyParser());
 var api = require(__dirname+'/router');
 app.all('*',function(req,res,next){
+    console.log(req.headers.origin);
     res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Access-Control-Allow-Credentials", "true"); //是否支持cookie跨域
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Max-Age", "1728000"); // cors预检请求的有效期（单位秒） 20天
     if(req.url == '/favicon.ico'){
         res.end();
     }
