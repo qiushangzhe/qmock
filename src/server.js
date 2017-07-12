4629d3fed41c5ea1136153e2066bb5204c3b9a5c
qiushangzhe <qiusz@jiqdaibao.com>
2017-07-12 14:19:05 +0800

    更新跨域问题的解决方案

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var api = require(__dirname+'/router');
 app.all('*',function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("Content-Type", "application/json;charset=utf-8");
