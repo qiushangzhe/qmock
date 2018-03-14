8948218f6ac723684a4841d3cd2128f44b613f2a
qiushangzhe <qiusz@jiqdaibao.com>
2018-03-14 18:46:05 +0800

    update

--- src/server.js
+++ src/server.js
@@ -1,29 +1,38 @@
 const Koa = require('koa');
 const cors = require('kcors');
 const app = new Koa();
-app.use(cors({
-  origin:'http://localhost:4200',
-  allowMethods:'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
-  credentials:true
-}));
+// app.use(cors({
+//   origin:'http://localhost:4200',
+//   allowMethods:'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+//   credentials:true
+// }));
 const fs = require('fs');
+
+app.use(async ctx => {
+    ctx.set('Access-Control-Allow-Origin', ctx.request.headers.host);
+    ctx.set('Access-Control-Allow-Credentials', true); // 是否支持cookie跨域
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+    ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    ctx.set('Access-Control-Max-Age', '1728000'); // cors预检请求的有效期（单位秒） 20天
+})
+
 app.use(async ctx => {
     ctx.url = ctx.url.split('?')[0];
     console.log(ctx.url);
     var jsonFile = getJsonFile(ctx.url);
-    if(!jsonFile){
+    if (!jsonFile) {
         ctx.status = 404;
-    }else{
+    } else {
         ctx.body = jsonFile;
     }
 });
 
-function getJsonFile(path){
+function getJsonFile(path) {
     var final_path = process.cwd() + '/mock' + path + '.json';
     console.log(final_path);
-    if(fs.existsSync(final_path)){
+    if (fs.existsSync(final_path)) {
         return fs.readFileSync(final_path).toString();
-    }else{
+    } else {
         return false;
     }
 }
