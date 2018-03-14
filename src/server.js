319cbf6be015fdd1a44918bebc3645c0a5f53830
qiushangzhe <qiusz@jiqdaibao.com>
2018-03-14 18:50:02 +0800

    fixbug

--- src/server.js
+++ src/server.js
@@ -8,12 +8,13 @@ const app = new Koa();
 // }));
 const fs = require('fs');
 
-app.use(async ctx => {
+app.use(async(ctx, next) => {
     ctx.set('Access-Control-Allow-Origin', ctx.request.headers.origin);
     ctx.set('Access-Control-Allow-Credentials', true); // 是否支持cookie跨域
     ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
     ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
     ctx.set('Access-Control-Max-Age', '1728000'); // cors预检请求的有效期（单位秒） 20天
+    await next();
 })
 
 app.use(async ctx => {
